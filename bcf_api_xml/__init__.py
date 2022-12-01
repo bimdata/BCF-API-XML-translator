3a64c4222106da3bef538f4995d500efbf17e1a9
Amoki <hugo@bimdata.io>
2022-12-01 15:25:10 +0100

    v0.2.14

--- bcf_api_xml/__init__.py
+++ bcf_api_xml/__init__.py
@@ -3,4 +3,4 @@ from bcf_api_xml.errors import UnsupportedBCFVersion  # noqa: F401
 from bcf_api_xml.export import to_zip  # noqa: F401
 from bcf_api_xml.import_zip import to_json  # noqa: F401
 
-__version__ = "0.2.13"
+__version__ = "0.2.14"

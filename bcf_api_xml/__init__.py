a4ef2b3f255af13f8b68603f4a6010cdea84939f
Amoki <hugo@bimdata.io>
2023-09-01 17:16:18 +0200

    release 0.4.3

--- bcf_api_xml/__init__.py
+++ bcf_api_xml/__init__.py
@@ -4,4 +4,4 @@ from bcf_api_xml.export import to_zip  # noqa: F401
 from bcf_api_xml.export import to_xlsx  # noqa: F401
 from bcf_api_xml.import_zip import to_json  # noqa: F401
 
-__version__ = "0.4.2"
+__version__ = "0.4.3"
